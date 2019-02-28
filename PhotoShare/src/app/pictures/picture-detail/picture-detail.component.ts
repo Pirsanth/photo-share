4a6be75c58b7393b73d888a2953b8e6dea7ed26b
Pirsanth <pirsanth@hotmail.com>
2019-02-28 22:09:11 +0800

    The code for the pictureDetail component. The like buttons work. And the post comment button works. The page dooes not refresh however

--- PhotoShare/src/app/pictures/picture-detail/picture-detail.component.ts
+++ PhotoShare/src/app/pictures/picture-detail/picture-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsService } from "../../services/comments.service";
+import { ActivatedRoute } from "@angular/router";
+import { CommentsDocument, commentObjectWithLikedBoolean } from "../../customTypes";
 
 @Component({
   selector: 'app-picture-detail',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picture-detail.component.css']
 })
 export class PictureDetailComponent implements OnInit {
+  commentsArray: commentObjectWithLikedBoolean[];
+  mainImageSrc: string;
+  albumName: string;
+  pictureTitle: string;
+  liked:boolean = true;
+  postingNewComment:boolean = false;
 
-  constructor() { }
+  constructor(private ajax:CommentsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    this.route.paramMap.subscribe( paramMap => {
+      this.albumName = paramMap.get("albumName");
+      this.pictureTitle = paramMap.get("pictureTitle");
 
+      this.ajax.getCommentDocument(this.albumName, this.pictureTitle).subscribe( (commentDoc:CommentsDocument<commentObjectWithLikedBoolean>) => {
+        console.log(commentDoc)
+        this.commentsArray = commentDoc.comments;
+        this.mainImageSrc = commentDoc.originalSrc;
+
+      })
+    })
+  }
+  likeComment(commentId:string){
+    this.ajax.likeComment(this.albumName, this.pictureTitle, commentId).subscribe(x => console.log("Comment like was successful added"))
+  }
+  unlikeComment(commentId:string){
+    this.ajax.removeLike(this.albumName, this.pictureTitle, commentId).subscribe(x => console.log("Comment like was successful removed"))
+  }
+  handleLikeButton(comment: commentObjectWithLikedBoolean){
+      if(comment.liked){
+        comment.likes--;
+        comment.liked = false;
+        this.unlikeComment(comment.commentId);
+      }
+      else{
+        comment.likes++;
+        comment.liked = true;
+        this.likeComment(comment.commentId);
+      }
+  }
+  handlePostComment(text:string){
+    this.postingNewComment = true;
+    this.ajax.addANewComment(this.albumName, this.pictureTitle, text)
+    .subscribe(x => this.postingNewComment = false);
+  }
+  print(){
+    console.log(this.commentsArray)
+  }
 }
