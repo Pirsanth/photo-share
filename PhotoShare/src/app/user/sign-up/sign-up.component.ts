806fe9783957b6baa98f786662c003034893f09c
Pirsanth <pirsanth@hotmail.com>
2019-03-24 11:22:32 +0800

    Finished the making the validation messages on the sign up form work
    I want the error messages for password to show even when repeat password is tampered with. However repeatPassword's error messages will only be shown when repeatPassword is tampered with
    
    The error of the passwords sub-formGroup will contain the validation message to be displayed by the UI.
    
    Added a lengthy comment describing the logic of the validator function
    
    Added a max length on the username for the signUp form

--- PhotoShare/src/app/user/sign-up/sign-up.component.ts
+++ PhotoShare/src/app/user/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from "../../services/authentication.service";
 import { Router } from "@angular/router";
+import { FormBuilder, Validators, FormGroup, ValidationErrors, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
@@ -11,11 +12,91 @@ export class SignUpComponent implements OnInit {
 
   previewSrc: string | ArrayBuffer;
   isValid:boolean = true;
-  constructor(private auth:AuthenticationService, private router:Router) { }
+  registrationForm = this.fb.group({
+      username: ["", Validators.required],
+      passwordGroup: this.fb.group({
+        password: ["", [Validators.required, Validators.minLength(4)] ],
+        repeatPassword: ["", [Validators.required, Validators.minLength(4)] ],
+      }, {validators: this.passswordsValidator}),
+      profilePicture: ["", [Validators.required] ]
+  })
+
+  constructor(private auth:AuthenticationService, private router:Router, private fb:FormBuilder) {}
 
   ngOnInit() {
   }
+  get profilePicture(){
+    return this.registrationForm.get("profilePicture");
+  }
+  get username(){
+    return this.registrationForm.get("username");
+  }
+  get passwordGroup(){
+    return this.registrationForm.get("passwordGroup");
+  }
+  get repeatPassword(){
+    return this.registrationForm.get("passwordGroup.repeatPassword");
+  }
 
+/*
+  The below validator function sets errors for the parent form group itself. The errors carry the validation messages to display in the UI
+  The validator fn also sets the passwordsDoNotMatch error on the repeatPassword child form control for CSS styling purposes
+
+  Valid and invalid in the if statements below simply refer to validity with respect to required and minlength, the validators on the child form controls themselves
+  (which is why we updateValueAndValidity if the customError passwordsDoNotMatch was set)
+
+  If both form controls are valid we check if the passwords match and display error messages accordingly.
+  If any of the two form controls are not valid, we just repeat the errors on the from controls in a user friendly message
+*/
+  passswordsValidator(group: FormGroup){
+    const password = group.get("password");
+    const repeatPassword = group.get("repeatPassword");
+
+    if(repeatPassword.invalid && "passwordsDoNotMatch" in repeatPassword.errors){
+      /*
+        if password is modified while repeatPassword has the customError, we reset the validity of repeatPassword with respect to the custom error
+        the validators on the repeatPassword form control still have to pass
+
+        Note: If repeatPassword is modified, the custom error gets shaken off
+      */
+      repeatPassword.updateValueAndValidity({onlySelf: true});
+    }
+
+    if( password.valid && repeatPassword.valid ){
+        if(password.value === repeatPassword.value){
+          return null;
+        }
+        else{
+          repeatPassword.setErrors({passwordsDoNotMatch: true});
+          return {repeatPassword: "Passwords do not match"};
+        }
+    }
+    else{
+        if( password.invalid && repeatPassword.invalid ){
+          return {
+                  password: makeErrorMessage("Password", password.errors),
+                  repeatPassword: makeErrorMessage("Repeat password", repeatPassword.errors)
+                }
+        }
+        else{
+          if(password.invalid){
+            return {password: makeErrorMessage("Password", password.errors)}
+          }
+          else{
+            return {repeatPassword: makeErrorMessage("Repeat password", repeatPassword.errors)};
+          }
+        }
+    }
+    function makeErrorMessage(controlName: string, error: ValidationErrors):string{
+        if(error.hasOwnProperty("required")){
+          return `${controlName} can't be left blank`;
+        }
+        //don't use else, be explicit
+        else if(error.hasOwnProperty("minlength")){
+          return `${controlName} must be at least ${error.minlength.requiredLength} characters long`;
+        }
+    }
+  }
   readFileIntoAURL(file:File){
     var fileReader = new FileReader();
       fileReader.readAsDataURL(file);
@@ -34,5 +115,11 @@ export class SignUpComponent implements OnInit {
      console.log("Sign up was a success")
      this.router.navigate(["/pictures"])
    });
+  }
+  touchedOrDirty(control: FormControl):boolean{
+    return control.dirty || control.touched
+  }
+  print(){
+    console.log(this.registrationForm.get("location").valid)
   }
 }
