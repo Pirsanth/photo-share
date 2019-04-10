096634dddfee7291ddcaa1813684e2b4a919cd4e
Pirsanth <pirsanth@hotmail.com>
2019-04-11 01:13:11 +0800

    *** Work on the frontend***
    ------------------------------------
    Added a new message component and message service to go with said
    component
    
    > Added a new message component whose purpose is to display feedback on
      form submissions and error messages unrelated to form validity
    
    > Right now, if we enter the add a new picture form without having
      signed in as a user, the error message is displayed in the form itself
      below the username field. I wish to seperate these errors from errors
      related to user input/the validity of the form.

---
+++ PhotoShare/src/app/message/message.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { MessageService } from "../services/message.service";
+import { Subject, Observable, fromEvent, merge, of } from "rxjs";
+import { map, scan, filter, mergeMap, takeUntil, tap, share, buffer, take } from "rxjs/operators";
+
+class messageObject {
+  animate:boolean = false;
+  constructor(public message:string){}
+}
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css'],
+})
+export class MessageComponent implements OnInit, OnDestroy {
+  @ViewChild("container") popupContainer;
+  messages: Observable<string>;
+  messageStack: Array<{message:string, animate:boolean}>;
+  messageCount:number = 0;
+  componentDestroyed: Subject<boolean> = new Subject();
+  movedPopups:number = 0;
+  constructor(private service: MessageService) { }
+
+  ngOnInit() {
+    if(this.service.isCacheFull()){
+      const stringArr = this.service.getCacheAndClear();
+      this.messageStack = stringArr.map( (message) => new messageObject(message));
+      this.messageCount = stringArr.length;
+    }
+  }
+  ngAfterViewInit() {
+
+    const start$ = fromEvent(this.popupContainer.nativeElement, "animationstart");
+    const end$ = fromEvent(this.popupContainer.nativeElement, "animationend");
+
+      start$
+      .pipe(
+        takeUntil(this.componentDestroyed)
+      )
+      .subscribe(x =>{
+          this.movedPopups++;
+      })
+
+    const noAnimationsStillRunning$ =
+      merge(
+        start$.pipe(map( (x)=> 1)),
+        end$.pipe(map( (x)=> -1))
+      ).pipe(
+        scan((acc, curr) => acc + curr, 0),
+        filter( (val)=> val === 0),
+        tap(() => {
+          /*
+            We do not want to change the array while the animations are running as
+            it will interrupt the animations if we remove the element we are currently animating.
+            At the same time, we want to clear the stack of clicked popups before
+            the code to add buffered popups (if there are any) is run
+          */
+          this.messageStack = this.messageStack.slice(0, this.messageStack.length - this.movedPopups);
+          this.movedPopups = 0;
+        }),
+        share(),
+      );
+
+      noAnimationsStillRunning$
+      .pipe(
+        takeUntil(this.componentDestroyed)
+      )
+      .subscribe()
+
+
+      this.service.messages$.pipe(
+              mergeMap( (value:string) => {
+                if(this.movedPopups){
+//for buffer to work the observable it pipes into must not complete immedially so the operator "of" can't be used
+                  const observable = new Observable((subscriber) => {
+                    subscriber.next(value);
+                  })
+                  return observable.pipe( buffer(noAnimationsStillRunning$), take(1), map((arr) => arr[0]) );
+                }
+                else{
+                  return of(value)
+                }
+              }),
+              takeUntil(this.componentDestroyed)
+          ).subscribe((message: string) =>{
+            /*
+              Adds new messages to the stack when animations have ended or when there are no ongoing animations
+            */
+            this.messageStack.push(new messageObject(message));
+            this.messageCount = this.messageStack.length;
+          });
+  }
+  ngOnDestroy(){
+    this.componentDestroyed.next(true);
+    this.componentDestroyed.complete();
+  }
+  decrementCount(indexOfClickedPopup:number){
+    if(!this.hasBeenClickedBefore(indexOfClickedPopup)){
+      this.messageCount--;
+    }
+  }
+  hasBeenClickedBefore(indexOfClickedPopup: number):boolean {
+    const numberOfUnclickedPopups = this.messageStack.length - this.movedPopups;
+    return indexOfClickedPopup + 1  > numberOfUnclickedPopups;
+  }
+}
---
+++ PhotoShare/src/app/services/message.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject, from, merge, Observable } from "rxjs";
+import {  concat } from "rxjs/operators";
+/*
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService implements OnDestroy{
+  private cachedMessages: string[];
+  private messageSubject: Subject<string> = new Subject();
+  messages$: Observable<string> = this.messageSubject.asObservable();
+
+ constructor() {}
+
+ ngOnDestroy(){
+   this.messageSubject.complete();
+ }
+ addToCache(message: string){
+   this.cachedMessages.push(message);
+ }
+ private clearCache():void{
+   this.cachedMessages = [];
+ }
+ getCacheAndClear():string[]{
+   const messages = this.cachedMessages;
+   this.clearCache();
+   return messages;
+ }
+ displayMessageImmediately(msg:string) {
+   this.messageSubject.next(msg);
+ }
+ isCacheFull():boolean{
+   return this.cachedMessages.length > 0;
+ }
+
+}
