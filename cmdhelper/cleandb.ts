889f492188d118c2dfefa47661f773b33afe2cac
Pirsanth <pirsanth@hotmail.com>
2019-04-16 22:46:53 +0800

    *** Updated the npm run reset command  ***
    --------------------------------------------------------------
    Npm run reset will now:
    
    > delete all the data on the database (all comments and user
    accounts will be lost)
    
    > all the public static files (the pictures, thumbnails and avatars)
    
    > It OS agnostic as it uses the path module
    
    > now it truly resets the contents of the app

--- cmdhelper/cleandb.ts
+++ cmdhelper/cleandb.ts
@@ -1,12 +1,25 @@
 import mongo from "mongodb"
-import {MongoClient} from "mongodb"
+import {MongoClient, Db} from "mongodb"
 
 var usedClient:MongoClient = null;
 
+//Its not really necessary to list them out one by one but I like to be explicit
+
 mongo.MongoClient.connect("mongodb://localhost:27017/photoShare", {useNewUrlParser: true})
 .then(client => {usedClient = client; return client.db()})
-.then(db => db.collection("albums"))
-.then(coll => coll.deleteMany({}))
-.then(()=> console.log("The albums db collection has been emptied"))
+.then( db => {
+  return Promise.all([ clearCollection("albums", db),
+                       clearCollection("users", db),
+                       clearCollection("comments", db),
+                       clearCollection("refreshTokens", db)
+                      ])
+})
+.then(()=> console.log("The PhotoShare db has been emptied"))
 .then(() => usedClient.close())
 .catch(e => console.log(e));
+
+async function clearCollection(collectionName:string, db:Db):Promise<boolean>{
+  var collection = await db.collection(collectionName)
+  await collection.deleteMany({})
+  return true;
+}
--- cmdhelper/del.ts
+++ cmdhelper/del.ts
@@ -1,18 +1,26 @@
 import FS from "fs";
 var fs = FS.promises;
+import path from "path";
 
-const originalsSrc = "../public/originals";
-const thumbsSrc = "../public/thumbnails";
+const publicDir = path.join(__dirname, "..", "public")
 
-Promise.all([cleanInsideTheDir(thumbsSrc), cleanInsideTheDir(originalsSrc)])
-.then(() => console.log("The public folder has been emptied"))
-.catch(err => console.log(err))
 
-function cleanInsideTheDir(dirPath: string):Promise<void[]>{
-  return fs.readdir(dirPath)
-          .then(arr => Promise.all(arr.map(filePath => removeOneFile(dirPath, filePath))))
+function cleanInsideTheDir(dirPath: string){
+ return fs.readdir(dirPath)
+          .then(arr => Promise.all(arr.map(fileName => removeOneFile(dirPath, fileName))))
 }
 
-function removeOneFile(dirPath:string, filepath: string):Promise<void>{
-  return fs.unlink(`${dirPath}/${filepath}`);
+function removeOneFile(dirPath:string, filename: string){
+  const pathToFile = path.join(dirPath, filename);
+  return fs.unlink(pathToFile);
 }
+
+async function cleanPublic(){
+  //we have to preserve the subfolders of the public directory
+  var dirsNotToDel =  await fs.readdir(publicDir);
+
+  await Promise.all(dirsNotToDel.map(dirName => cleanInsideTheDir(path.join(publicDir, dirName))));
+  console.log("Static files in the public folder cleared");
+
+}
+cleanPublic();
